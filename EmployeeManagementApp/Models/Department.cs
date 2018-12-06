d2ead49d2a6b1051ca5bc2c6f56247f8e8712af2
tapu007 <tapukumar311@gamil.com>
2018-12-06 21:16:14 +0600

    code property is added in department class

--- EmployeeManagementApp/Models/Department.cs
+++ EmployeeManagementApp/Models/Department.cs
@@ -10,6 +10,8 @@ namespace EmployeeManagementApp.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public List<Employee>Employees { get; set; }
+        public  string Code { get; set; }
+
 
     }
 }
