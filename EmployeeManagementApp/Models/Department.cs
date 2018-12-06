b983800319c5104ad7a440189ce6dbf30ffc56b3
tapu007 <tapukumar311@gamil.com>
2018-12-06 20:59:18 +0600

    list of employee added on departmetn class

--- EmployeeManagementApp/Models/Department.cs
+++ EmployeeManagementApp/Models/Department.cs
@@ -9,5 +9,7 @@ namespace EmployeeManagementApp.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public List<Employee>Employees { get; set; }
+
     }
 }
